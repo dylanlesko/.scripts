f0d3c7e877350838a908a608118845de48d6bf6e
dylan <dylan@lesko.io>
2015-10-31 18:12:58 -0400

    initial scripts

---
+++ touchlock/touchlock.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/bash
+
+#too lazy to find the id of the device
+#idvar=$(xinput list | grep 'Synaptics')
+#echo $idvar | grep '^id='
+#echo $idvar | cut -c 39-40
+
+
+
+test=$(xinput list-props 13 | grep 'Device Enabled')
+test=$(echo $test|awk '{print substr($0,length($0),1)}')
+
+if [ $test == 0 ]
+then
+	xinput set-prop 13 "Device Enabled" 1
+else
+	xinput set-prop 13 "Device Enabled" 0
+fi
