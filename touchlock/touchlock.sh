f116306f75e5bb48cf0063d88a47ee8ce210e953
dylan <dylan@lesko.io>
2015-11-02 13:51:56 -0500

    Dynamically fetch id of synaptics device

--- touchlock/touchlock.sh
+++ touchlock/touchlock.sh
@@ -1,18 +1,18 @@
 #!/usr/bin/bash
 
 #too lazy to find the id of the device
-#idvar=$(xinput list | grep 'Synaptics')
-#echo $idvar | grep '^id='
-#echo $idvar | cut -c 39-40
+idvar=$(xinput list | grep 'Synaptics')
+echo $idvar | grep '^id='
+id=$(echo $idvar | cut -c 39-40)
 
-
+#echo $id
 
 test=$(xinput list-props 13 | grep 'Device Enabled')
 test=$(echo $test|awk '{print substr($0,length($0),1)}')
 
 if [ $test == 0 ]
 then
-	xinput set-prop 13 "Device Enabled" 1
+	xinput set-prop $id "Device Enabled" 1
 else
-	xinput set-prop 13 "Device Enabled" 0
+	xinput set-prop $id "Device Enabled" 0
 fi
